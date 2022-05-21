c1eb40d2354a8105566b227d3abe6489d9c71ce3
wada <wada@avita.co.jp>
2022-05-21 18:11:19 +0900

    split process into functions

--- src/index.js
+++ src/index.js
@@ -3,12 +3,46 @@ const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
 const NPB_OFFICIAL_URL = 'https://npb.jp/';
 const NPB_OFFICIAL_TITLE = 'NPB.jp 日本野球機構';
 
-(async function example() {
-  let driver = await new Builder().forBrowser(Browser.SAFARI).build();
+/**
+ * Safari 用の driver をセットアップする
+ * @param {*} driver
+ * @returns {*}
+ */
+async function setupDriverForSafari() {
+  const driver = await new Builder().forBrowser(Browser.SAFARI).build();
+  return driver;
+}
+
+/**
+ * NPB 公式サイトへアクセスする
+ * @param {*} driver
+ */
+async function accessNPBOfficialSite(driver) {
+  await driver.get(NPB_OFFICIAL_URL);
+}
+
+/**
+ * タイトルがロードされるまで待機
+ * @param {*} driver
+ */
+async function waitUntilTitleLoaded(driver) {
+  await driver.wait(until.titleIs(NPB_OFFICIAL_TITLE), 3000);
+}
+
+/**
+ * スクレイピングの終了
+ * @param {*} driver
+ */
+async function finishScraping(driver) {
+  await driver.quit();
+}
+
+(async function main() {
+  let driver = await setupDriverForSafari();
   try {
-    await driver.get(NPB_OFFICIAL_URL);
-    await driver.wait(until.titleIs(NPB_OFFICIAL_TITLE), 3000);
+    await accessNPBOfficialSite(driver);
+    await waitUntilTitleLoaded(driver);
   } finally {
-    await driver.quit();
+    await finishScraping(driver);
   }
 })();
