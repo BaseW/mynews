52e15257ad81ad5979dbc0f061536bd80beda984
wada <wada@avita.co.jp>
2022-05-22 16:35:42 +0900

    notify info from organized result

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,82 @@
 const FIREBASE_FUNCTIONS_URL = "https://asia-northeast1-quickstart-1587635856027.cloudfunctions.net/scrapingNPB"
 const SLACK_WEBHOOK_URL = PropertiesService.getScriptProperties().getProperty("SLACK_WEBHOOK_URL");;
 
+export type ResultType = {
+  dateInfo: string;
+  gameInfoList: GameInfo[];
+}
+
+export type GameInfo = {
+  leftTeamName: string;
+  rightTeamName: string;
+  leftTeamScore: string;
+  rightTeamScore: string;
+  gameStateInfo: string;
+}
+
+export type ParsedScoreInfo = {
+  leftTeamScore: string;
+  rightTeamScore: string;
+}
+
+
 /**
  * 試合情報の取得
- * @returns {string}
+ * @returns {ResultType}
  */
-function getGameInfo(): string {
-  const gameInfo = UrlFetchApp.fetch(FIREBASE_FUNCTIONS_URL).getContentText();
+function getGameInfo(): ResultType {
+  const rawGameInfo: string = UrlFetchApp.fetch(FIREBASE_FUNCTIONS_URL).getContentText();
+  const gameInfo: ResultType = JSON.parse(rawGameInfo);
   return gameInfo;
 }
 
 /**
- * Slack への通知
- * @param {string} gameinfo
+ * 試合情報の整理
+ * @param {ResultType} fetchedResult
+ * @returns {string}
  */
-function postToSlack(gameInfo: string) {
+function organizeGameInfo(fetchedResult: ResultType): string {
+  const {
+    dateInfo,
+    gameInfoList,
+  } = fetchedResult;
+  let organizedGameInfo = `${dateInfo}\n`;
+  console.log(gameInfoList);
+  for (let i = 0; i < gameInfoList.length; i ++) {
+    const gameInfo: GameInfo = gameInfoList[i];
+    const {
+      leftTeamName,
+      rightTeamName,
+      leftTeamScore,
+      rightTeamScore,
+      gameStateInfo
+    } = gameInfo;
+    if (leftTeamName && rightTeamName) {
+      if (leftTeamScore && rightTeamScore) {
+        const gameInfo = `${leftTeamName} ${leftTeamScore} - ${rightTeamScore} ${rightTeamName}`;
+        if (gameStateInfo) {
+          organizedGameInfo += gameInfo + gameStateInfo;
+        }
+      } else {
+        const gameInfo = `${leftTeamName} vs ${rightTeamName}`;
+        if (gameStateInfo) {
+          organizedGameInfo += gameInfo + gameStateInfo;
+        }
+        organizedGameInfo += gameInfo;
+      }
+    }
+    organizedGameInfo += "\n";
+  }
+  return organizedGameInfo;
+}
+
+/**
+ * Slack への通知
+ * @param {string} organizedGameInfo
+ */
+function postToSlack(organizedGameInfo: string) {
   const payload = {
-    text: gameInfo
+    text: organizedGameInfo
   }
   const options = {
     'method' : 'post',
@@ -33,7 +93,10 @@ function postToSlack(gameInfo: string) {
 function main() {
   try {
     const gameInfo = getGameInfo();
-    postToSlack(gameInfo);
+    console.log(gameInfo);
+    const organizedGameInfo = organizeGameInfo(gameInfo);
+    console.log(organizedGameInfo);
+    postToSlack(organizedGameInfo);
   } catch (error) {
     console.log(error);
   }
