74e0e559eb3392177f9a081d779fa25671625223
wada <wada@avita.co.jp>
2022-06-04 22:17:26 +0900

    update type

--- src/types/line.ts
+++ src/types/line.ts
@@ -3,11 +3,41 @@ export type LineRequestPostDataContentEventSource = {
   groupId: string;
 }
 
+export enum LineRequestPostDataContentEventType {
+  Message = "message",
+}
+
+export enum LineRequestPostDataContentEventMessageType {
+  Text = "text",
+}
+
+export type LineRequestPostDataContentEventMessage = {
+  type: LineRequestPostDataContentEventMessageType;
+  id: string;
+  text: string;
+}
+
+export type DeliveryContext = {
+  isRedelivery: boolean;
+}
+
+export enum LineRequestPostDataContentEventMode {
+  Active = "active",
+}
+
 export type LineRequestPostDataContentEvent = {
+  type: LineRequestPostDataContentEventType;
+  message: LineRequestPostDataContentEventMessage;
+  webhookEventId: string;
+  deliveryContext: DeliveryContext;
+  timestamp: number;
   source: LineRequestPostDataContentEventSource;
+  replyToken: string;
+  mode: LineRequestPostDataContentEventMode;
 }
 
 export type LineRequestPostDataContent = {
+  destination: string;
   events: LineRequestPostDataContentEvent[];
 }
 
