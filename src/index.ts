b273e7ed28fac2bd3f98d6df083f788f11d1e378
wada <wada@avita.co.jp>
2022-06-07 23:19:37 +0900

    messages for multiple users

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import {
 } from "./types";
 
 const LINE_CHANNEL_ACCESS_TOKEN_HOME = PropertiesService.getScriptProperties().getProperty("LINE_CHANNEL_ACCESS_TOKEN_HOME");
-const LINE_USER_ID = PropertiesService.getScriptProperties().getProperty("LINE_USER_ID");
+const LINE_USER_1_ID = PropertiesService.getScriptProperties().getProperty("LINE_USER_1_ID");
+const LINE_USER_2_ID = PropertiesService.getScriptProperties().getProperty("LINE_USER_2_ID");
+const MESSAGE_FOR_USER_1 = PropertiesService.getScriptProperties().getProperty("MESSAGE_FOR_USER_1");
+const MESSAGE_FOR_USER_2 = PropertiesService.getScriptProperties().getProperty("MESSAGE_FOR_USER_2");
 const LINE_API_PUSH_MESSAGE_URL = "https://api.line.me/v2/bot/message/push";
 
-const validUserIdList = [LINE_USER_ID];
+const validUserIdList = [LINE_USER_1_ID, LINE_USER_2_ID];
 // const validUserIdList = [];
 
 function sendErrorResponse(userId: string) {
@@ -35,7 +38,11 @@ function sendErrorResponse(userId: string) {
 
 function sendOkResponse(userId: string) {
   const url = LINE_API_PUSH_MESSAGE_URL;
-  const body = "Valid Request";
+  const body = userId === LINE_USER_1_ID
+    ? MESSAGE_FOR_USER_1
+    : userId === LINE_USER_2_ID
+      ? MESSAGE_FOR_USER_2
+      : "Invalid user";
   try {
     UrlFetchApp.fetch(url, {
       'headers': {
